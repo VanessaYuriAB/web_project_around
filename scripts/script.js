0ffd0028c14e079b578c30dd331425cd5085a691
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-03-05 08:29:35 -0300

    feat: opens the editing form filled with profile information

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,17 @@
-/*para abrir o formulário*/
+/*para abrir o formulário com as infos do perfil*/
 
 let editBtn = document.querySelector(".infos__edit-btn");
 let popupBox = document.querySelector(".popup");
 
 function openPopup() {
   popupBox.classList.toggle("popup_opened");
+
+  let inputsPopups = popupBox.querySelectorAll(".popup__input-form");
+  let nameProfile = document.querySelector(".infos__name");
+  let aboutProfile = document.querySelector(".infos__about");
+
+  inputsPopups[0].value = nameProfile.textContent;
+  inputsPopups[1].value = aboutProfile.textContent;
 }
 
 editBtn.addEventListener("click", openPopup);
@@ -21,3 +28,40 @@ function closePopup() {
 }
 
 closeBtn.addEventListener("click", closePopup);
+
+/*para salvar as informações*/
+
+/* CÓDIGO DA PLATAFORMA PARA O EVENTO ESPECIAL SUBMIT:
+
+// Vamos encontrar o formulário no DOM
+let formElement = // Use o método querySelector()
+
+// Em seguida vem o handler do submit
+// ainda não vai enviar para lugar nenhum
+
+// Observe que o nome da função começa com um verbo
+// e descreve exatamente o que a função faz
+function handleProfileFormSubmit(evt) {
+    // Esta linha impede o navegador
+    // de enviar o formulário da forma padrão.
+    evt.preventDefault();
+    // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+    // Explicaremos em mais detalhes posteriormente.
+
+    // Vamos encontrar os campos de formulário do DOM
+    let nameInput = // Use querySelector()
+    let jobInput = // Use querySelector()
+
+    // Pegue os valores de cada campo do valor da propriedade correspondente
+
+    // Selecione os elementos aos quais os valores dos campos serão inseridos
+
+    // Insira novos valores usando a
+    // propriedade textContent
+}
+
+// Conecte o handler ao formulário:
+// ele vai observar o evento de submit
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
+*/
