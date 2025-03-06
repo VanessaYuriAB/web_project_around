7912cf04314aadc03c6a16b58cf30ffc2088dd30
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-03-06 05:29:10 -0300

    feat: saves form information to user profile

--- scripts/script.js
+++ scripts/script.js
@@ -31,10 +31,10 @@ closeBtn.addEventListener("click", closePopup);
 
 /*para salvar as informações*/
 
-/* CÓDIGO DA PLATAFORMA PARA O EVENTO ESPECIAL SUBMIT:
+/* CÓDIGO DA PLATAFORMA PARA O EVENTO ESPECIAL SUBMIT: */
 
 // Vamos encontrar o formulário no DOM
-let formElement = // Use o método querySelector()
+let formElement = document.querySelector(".popup");
 
 // Em seguida vem o handler do submit
 // ainda não vai enviar para lugar nenhum
@@ -42,26 +42,41 @@ let formElement = // Use o método querySelector()
 // Observe que o nome da função começa com um verbo
 // e descreve exatamente o que a função faz
 function handleProfileFormSubmit(evt) {
-    // Esta linha impede o navegador
-    // de enviar o formulário da forma padrão.
-    evt.preventDefault();
-    // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
-    // Explicaremos em mais detalhes posteriormente.
+  // Esta linha impede o navegador
+  // de enviar o formulário da forma padrão.
+  evt.preventDefault();
+  // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+  // Explicaremos em mais detalhes posteriormente.
 
-    // Vamos encontrar os campos de formulário do DOM
-    let nameInput = // Use querySelector()
-    let jobInput = // Use querySelector()
+  // Vamos encontrar os campos de formulário do DOM
+  let inputsForm = formElement.querySelectorAll(".popup__input-form");
+  let nameInput = inputsForm[0]; // Use querySelector()
+  let jobInput = inputsForm[1]; // Use querySelector()
 
-    // Pegue os valores de cada campo do valor da propriedade correspondente
+  // Pegue os valores de cada campo do valor da propriedade correspondente
+  nameInput = nameInput.value;
+  jobInput = jobInput.value;
 
-    // Selecione os elementos aos quais os valores dos campos serão inseridos
+  // Selecione os elementos aos quais os valores dos campos serão inseridos
+  let nameProfile = document.querySelector(".infos__name");
+  let jobProfile = document.querySelector(".infos__about");
 
-    // Insira novos valores usando a
-    // propriedade textContent
+  // Insira novos valores usando a
+  // propriedade textContent
+  nameProfile.textContent = nameInput;
+  jobProfile.textContent = jobInput;
+
+  alert("As informações do perfil foram atualizadas com sucesso!");
+
+  let closeForm = document.querySelector(".popup__close-btn");
+
+  function closePopup() {
+    popupBox.classList.toggle("popup_opened");
+  }
+
+  closePopup();
 }
 
 // Conecte o handler ao formulário:
 // ele vai observar o evento de submit
-formElement.addEventListener('submit', handleProfileFormSubmit);
-
-*/
+formElement.addEventListener("submit", handleProfileFormSubmit);
