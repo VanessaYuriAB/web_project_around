c41c94f1f55c47580ec36d34a51ee467f8a83695
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-04-09 12:25:47 -0300

    feat: adds configuration objects for validation, modular javascript and defines attribute for buttons

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,27 +1,32 @@
 // para validar os formulários (edt e add)
 
 // o que acontece quando o campo não está válido? obs: a classe do input foi programada com :invalid
-function showInputError(formElement, inputElement, errorMessage) {
+function showInputError(formElement, inputElement, errorMessage, config) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 
   errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup-edt__input-error");
+  errorElement.classList.add(config.inputErrorClass);
 }
 
 // o que acontece quando o campo está válido?
-function hideInputError(formElement, inputElement) {
+function hideInputError(formElement, inputElement, config) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 
-  errorElement.classList.remove("popup-edt__input-error");
+  errorElement.classList.remove(config.inputErrorClass);
   errorElement.textContent = "";
 }
 
 // verificando se o campo está válido ou não para exibir msgs de erro ao usuário
-function checkInputValidity(formElement, inputElement) {
+function checkInputValidity(formElement, inputElement, config) {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      config
+    );
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, config);
   }
 }
 
@@ -33,44 +38,64 @@ function hasInvalidInput(inputList) {
 }
 
 // alternando estados dos botões conforme estados dos campos
-function toggleButtonState(inputList, buttonElement) {
+function toggleButtonState(inputList, buttonElement, config) {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("popup__btn-form_disabled");
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.disabled = true;
   } else {
-    buttonElement.classList.remove("popup__btn-form_disabled");
+    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.disabled = false;
   }
 }
 
 // verificando todos os campos para condicionar o botão
-function setEventListeners(formElement) {
-  const inputs = Array.from(formElement.querySelectorAll("input"));
-  const button = formElement.querySelector("button");
+function setEventListeners(formElement, config) {
+  const inputs = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const button = formElement.querySelector(config.submitButtonSelector);
 
-  toggleButtonState(inputs, button);
+  toggleButtonState(inputs, button, config);
 
   inputs.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputs, button);
+      checkInputValidity(formElement, inputElement, config);
+      toggleButtonState(inputs, button, config);
     });
   });
 }
 
-// ativando verificação total para todos os formulários
-function enableValidation() {
-  const forms = Array.from(document.querySelectorAll("form"));
+// verificação total para todos os formulários
+function enableValidation(config) {
+  const forms = Array.from(document.querySelectorAll(config.formSelector));
 
   forms.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
     });
 
-    setEventListeners(formElement);
+    setEventListeners(formElement, config);
   });
 }
 
-enableValidation();
+// objetos de configuração
+const configEdt = {
+  formSelector: ".popup-edt__container",
+  inputSelector: ".popup-edt__input-form",
+  submitButtonSelector: ".popup-edt__btn-form",
+  inactiveButtonClass: "popup-edt__btn-form_disabled",
+  inputErrorClass: "popup-edt__input-error",
+};
+
+const configAdd = {
+  formSelector: ".popup-add__container",
+  inputSelector: ".popup-add__input-form",
+  submitButtonSelector: ".popup-add__btn-form",
+  inactiveButtonClass: "popup-add__btn-form_disabled",
+  inputErrorClass: "popup-add__input-error",
+};
+
+enableValidation(configEdt);
+enableValidation(configAdd);
 
 // export
 
-export { toggleButtonState, hideInputError };
+export { toggleButtonState, hideInputError, configEdt, configAdd };
