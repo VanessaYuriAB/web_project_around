0a8373e62c343af6061810104ce933c0dc5e3452
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-05-14 13:33:44 -0300

    refactor: finalizes new classes: Section and UserInfo

--- components/Section.js
+++ components/Section.js
@@ -1,20 +1,20 @@
-//não tem marcação. Ela recebe marcação por meio da função de retorno de chamada e insere no contêiner.
-class Section {
-  //objeto com duas propriedades (items e renderer) como primeiro parâmetro do construtor. A propriedade items serve como um vetor de dados, que você precisa adicionar em uma página ao inicializar a classe. A propriedade renderer é uma função responsável por criar e renderizar dados em uma página. O segundo parâmetro deve ser um seletor de classe CSS onde você adicionará os elementos do cartão.
+//Não tem marcação, recebe marcação por meio da função de retorno de chamada e insere no contêiner.
+export default class Section {
+  //Objeto com duas propriedades (items e renderer) como primeiro parâmetro do construtor. A propriedade items serve como um vetor de dados, que você precisa adicionar em uma página ao inicializar a classe. A propriedade renderer é uma função responsável por criar e renderizar dados em uma página. O segundo parâmetro deve ser um seletor de classe CSS onde você adicionará os elementos do cartão.
   constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
-  //renderiza todos os elementos na página. A função renderer() vai renderizar cada elemento em uma página.
+  //Renderiza todos os elementos na página. A função renderer() vai renderizar cada elemento em uma página.
   renderer() {
     this._items.forEach((item) => {
       this._renderer(item);
     });
   }
 
-  //pega um elemento DOM e adiciona ao contêiner.
+  //Pega um elemento DOM e adiciona ao contêiner.
   addItem(element) {
     this._container.append(element);
   }
--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,27 +1,22 @@
-//responsável por renderizar a informação sobre o usuário na página.
+//Responsável por renderizar a informação sobre o usuário na página.
 class UserInfo {
-  //objeto: um contendo o nome do usuário e o outro contendo o trabalho do usuário.
-  constructor({ name, job }) {
-    //Pegar um objeto com os seletores de dois elementos no construtor.
-    this._name = document.querySelector(name);
-    this._job = document.querySelector(job);
+  //Pegar um objeto com os seletores de dois elementos no construtor: um contendo o nome do usuário e o outro contendo o trabalho do usuário.
+  constructor({ nameSelector, jobSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._jobElement = document.querySelector(jobSelector);
   }
 
-  //retorna um objeto com informação sobre o usuário. Esse método será útil para casos em que é necessário exibir os dados do usuário no formulário aberto.
+  //Retorna um objeto com informação sobre o usuário. Esse método será útil para casos em que é necessário exibir os dados do usuário no formulário aberto.
   getUserInfo() {
-    // para o popup edt: preenche inputs com infos do perfil
-    const popupInputs = Array.from(
-      popupBox.querySelectorAll(config.inputSelector)
-    );
-
-    if (extraFunctions && extraFunctions.fillEdtPopupInputs) {
-      extraFunctions.fillEdtPopupInputs(popupInputs);
-    }
-    // VERIFICAR FUNÇÕES, POIS ESTÁ DE ACORDO COM CÓDIGO ANTIGO
+    return {
+      name: this._nameElement.textContent,
+      job: this._jobElement.textContent,
+    };
   }
 
-  //pega novos dados do usuário e adiciona na página.
-  setUserInfo() {
-    // SOMENTE INFOS DO PERFIL?
+  //Pega novos dados do usuário e adiciona na página.
+  setUserInfo({ name, job }) {
+    this._nameElement.textContent = name;
+    this._jobElement.textContent = job;
   }
 }
