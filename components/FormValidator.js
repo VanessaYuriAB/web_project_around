8a3fca407fd378f802556c70d9214d1e67ce4d80
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-05-17 16:12:32 -0300

    refactor: remove submit listener with .preventDefault() (duplicate code)

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -69,8 +69,6 @@ export default class FormValidator {
 
   // habilita a validação do formulário
   enableValidation() {
-    this._formElement.addEventListener("submit", (evt) => evt.preventDefault());
-
     this._setEventListeners();
   }
 }
