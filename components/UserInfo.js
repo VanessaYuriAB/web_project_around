fb28272d4986a26a5dad22c99ba0495e8fbcd617
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-05-20 15:42:55 -0300

    refactor: change property key name (from job to about) and modify comments

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,21 +1,21 @@
 export default class UserInfo {
-  //Pegar um objeto com os seletores de dois elementos no construtor: um contendo o nome do usuário e o outro contendo o trabalho do usuário.
-  constructor({ nameSelector, jobSelector }) {
+  //Pega um objeto com os seletores de dois elementos no construtor: um contendo o nome do usuário e o outro contendo o trabalho do usuário.
+  constructor({ nameSelector, aboutSelector }) {
     this._nameElement = document.querySelector(nameSelector);
-    this._jobElement = document.querySelector(jobSelector);
+    this._aboutElement = document.querySelector(aboutSelector);
   }
 
-  //Retorna um objeto com informação sobre o usuário. Esse método será útil para casos em que é necessário exibir os dados do usuário no formulário aberto.
+  //Retorna um objeto com informação sobre o usuário. Método útil para exibir os dados do usuário no formulário aberto.
   getUserInfo() {
     return {
       name: this._nameElement.textContent,
-      job: this._jobElement.textContent,
+      about: this._aboutElement.textContent,
     };
   }
 
   //Pega novos dados do usuário e adiciona na página.
-  setUserInfo({ name, job }) {
+  setUserInfo({ name, about }) {
     this._nameElement.textContent = name;
-    this._jobElement.textContent = job;
+    this._aboutElement.textContent = about;
   }
 }
