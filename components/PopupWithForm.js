e729f6981eb51007d7807d09dd664077cf39e6ab
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-05-20 15:45:40 -0300

    chore: change comments and delete duplicate constant

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -4,15 +4,18 @@ export default class PopupWithForm extends Popup {
   //Função de retorno de chamada do envio do formulário como parâmetro do construtor, assim como o seletor do pop-up.
   constructor(popupSelector, handleSubmit) {
     super(popupSelector);
-    this._formElement = this._element.querySelector(this._config.formSelector);
+
     this._submitBtnElement = this._element.querySelector(
       this._config.submitButtonSelector
     );
+
+    // dados já são ajustados aqui
     this._handleSubmit = (evt) => {
       evt.preventDefault();
       handleSubmit(this._getInputValues());
       this.close();
     };
+
     this._formInputs = Array.from(
       this._formElement.querySelectorAll(this._config.inputSelector)
     );
@@ -30,7 +33,6 @@ export default class PopupWithForm extends Popup {
     return this._formInputValues;
   }
 
-  //Modifica o método pai close(): redefine o formulário assim que o pop-up é fechado.
   close() {
     super.close();
 
@@ -42,7 +44,6 @@ export default class PopupWithForm extends Popup {
     }
   }
 
-  //Modifica o método pai setEventListeners(): adiciona o manipulador de eventos Submit ao formulário e o ouvinte de eventos click para o ícone de fechamento.
   setEventListeners() {
     super.setEventListeners();
 
