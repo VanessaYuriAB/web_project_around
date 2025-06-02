5080ce3fef4d53ffdf405507071cfe1d40db2929
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-06-02 11:50:23 -0300

    feat: add constants for profile edit photo

--- utils/constants.js
+++ utils/constants.js
@@ -41,7 +41,7 @@ const profileSelectors = {
   about: ".infos__about",
 };
 
-// objetos de configuração para formulários edt, add e trash
+// objetos de configuração para formulários edt, add, trash e photo
 const configEdt = {
   boxFormSelector: ".popup-edt",
   formSelector: ".popup-edt__container",
@@ -72,9 +72,21 @@ const configTrash = {
   closedPopupClass: "popup-trash_closed",
 };
 
+const configPhoto = {
+  boxFormSelector: ".popup-photo",
+  formSelector: ".popup-photo__container",
+  inputSelector: ".popup-photo__input-form",
+  submitButtonSelector: ".popup-photo__btn-form",
+  closeButtonSelector: ".popup-photo__icon-close-btn",
+  closedPopupClass: "popup-photo_closed",
+  inactiveButtonClass: "popup-photo__btn-form_disabled",
+  inputErrorClass: "popup-photo__input-error",
+};
+
 // constantes para validador
 const edtFormElement = document.querySelector(configEdt.formSelector);
 const addFormElement = document.querySelector(configAdd.formSelector);
+const photoFormElement = document.querySelector(configPhoto.formSelector);
 
 // constantes para campos do popup form edt
 const nameInput = document.querySelector('input[name="name"]');
@@ -97,9 +109,10 @@ const configCard = {
 // objeto para handlers dos listeners de fechamento dos popups
 const popupHandlers = { clickOut: null, keyEsc: null, closeBtn: null };
 
-// constantes para botões de popups form
+// constantes para botões de abertura dos popups form
 const edtBtnElement = document.querySelector(".infos__edit-btn");
 const addBtnElement = document.querySelector(".profile__add-btn");
+const photoBtnElement = document.querySelector(".profile__photo");
 
 export {
   templateCards,
@@ -118,4 +131,7 @@ export {
   templatePopupImg,
   configCard,
   configTrash,
+  configPhoto,
+  photoFormElement,
+  photoBtnElement,
 };
