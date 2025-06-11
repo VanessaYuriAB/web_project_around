936b1071f06ee290be8e909933bfdf54a480ee8c
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-06-11 11:00:04 -0300

    feat: add class PopupWithConfirmation (js)

---
+++ components/PopupWithConfirmation.js
@@ -0,0 +1,44 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithConfirmation extends Popup {
+  constructor(popupSelector, handleConfirm) {
+    super(popupSelector);
+
+    this._currentCard = null;
+    this._currentCardId = null;
+
+    this._confirmBtnElement = this._element.querySelector(
+      this._config.submitButtonSelector
+    );
+
+    this._originalBtnText = this._confirmBtnElement.textContent; // salva o texto original do botão
+
+    // dados já são ajustados aqui
+    this._handleConfirm = (evt) => {
+      evt.preventDefault();
+      this.renderLoading(true);
+      handleConfirm(this._currentCard, this._currentCardId);
+      this.close();
+    };
+
+    this._formElement.addEventListener("submit", this._handleConfirm);
+  }
+
+  // altera método open da classe mãe, inserindo parâmentros
+  open(cardElement, cardId) {
+    super.open();
+    this._currentCard = cardElement;
+    this._currentCardId = cardId;
+  }
+
+  // informa usuário sobre progresso da solicitação
+  renderLoading(isLoading) {
+    if (isLoading) {
+      // altera texto do botão
+      this._confirmBtnElement.textContent = "Salvando...";
+    } else {
+      // retorna texto original
+      this._confirmBtnElement.textContent = this._originalBtnText;
+    }
+  }
+}
